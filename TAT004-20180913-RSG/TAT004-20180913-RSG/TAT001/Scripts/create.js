9a85e0e0710645e7cee760c5073515497f7c9139
Munder90 <43380406+Munder90@users.noreply.github.com>
2018-09-18 10:53:14 -0500

    Conflicto2

--- TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Scripts/create.js
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Scripts/create.js
@@ -3,6 +3,7 @@ var monedafinanciera = true;
 var negdistribucion = true;
 var disdistribucion = true;
 // PARA GIT
+// Omar
 var interval; //B20180625 MGC 2018.07.04
 var borradorinac = 300000; //B20180625 MGC 2018.07.04 Tiempo de espera de inactividad 5 minutos
 //var borradorinac = 60000; //B20180625 MGC 2018.07.04 Tiempo de espera de inactividad 1 minuto
