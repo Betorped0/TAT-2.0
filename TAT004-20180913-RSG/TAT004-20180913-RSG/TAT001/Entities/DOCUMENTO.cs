95200ecb16cd04be920ee4f3cc505a9c6163a41f
Rogelio Sánchez Gómez <36237248+sf-rogeliosanchez@users.noreply.github.com>
2018-09-24 11:49:26 -0500

    Cambios RSG

--- TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Entities/DOCUMENTO.cs
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Entities/DOCUMENTO.cs
@@ -31,6 +31,8 @@ namespace TAT001.Entities
         public List<TAT001.Models.DOCUMENTOP_MOD> DOCUMENTOP { get; set; }
         public List<DOCUMENTOF> DOCUMENTOF { get; set; }
         public List<DOCUMENTOREC> DOCUMENTOREC { get; set; }
+        public List<DOCUMENTORAN> DOCUMENTORAN { get; set; }
+
         public decimal NUM_DOC { get; set; }
         public string TSOL_ID { get; set; }
         public string TALL_ID { get; set; }
