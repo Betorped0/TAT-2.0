9ec0040162bc24551157435858885a307239fdc4
Rogelio Sánchez Gómez <36237248+sf-rogeliosanchez@users.noreply.github.com>
2018-10-05 10:35:14 -0500

    20181005-RSG

--- TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Models/AlertaViewModel.cs
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Models/AlertaViewModel.cs
@@ -14,21 +14,32 @@ namespace TAT001.Models
             alertas = new List<WARNINGP>();
             alerta = new WARNINGP();
             alertaMensajes = new List<WARNINGPT>();
+            alertaCondiciones = new List<WARNING_COND>();
 
             sociedades = new List<SelectListItem>();
             tabs = new List<SelectListItem>();
             tiposSolicitud = new List<SelectListItem>();
             campos = new List<SelectListItem>();
             tipos = new List<SelectListItem>();
+            condCampos = new List<SelectListItem>();
+            condValores = new List<SelectListItem>();
+            condCampos1 = new List<SelectListItem>();
+            condValores1 = new List<SelectListItem>();
 
         }
         public List<WARNINGP> alertas { get; set; }
         public WARNINGP alerta { get; set; }
         public List<WARNINGPT> alertaMensajes { get; set; }
+        public List<WARNING_COND> alertaCondiciones { get; set; }
+
         public List<SelectListItem> sociedades { get; set; }
         public List<SelectListItem> tiposSolicitud { get; set; }
         public List<SelectListItem> tabs { get; set; }
         public List<SelectListItem> campos { get; set; }
         public List<SelectListItem> tipos { get; set; }
+        public List<SelectListItem> condCampos { get; set; }
+        public List<SelectListItem> condValores { get; set; }
+        public List<SelectListItem> condCampos1 { get; set; }
+        public List<SelectListItem> condValores1 { get; set; }
     }
 }
--- TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Models/ReportesModel.cs
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Models/ReportesModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TAT001.Entities;
 
 namespace TAT001.Models
 {
@@ -31,11 +32,12 @@ namespace TAT001.Models
             public double TIEMPO_TRANSCURRIDO { get; set; }
             public int SEMANA { get; set; }
             public int NUMERO_CORRECCIONES { get; set; }
-            public string STATUS_STRING {
+            public string STATUS_STRING
+            {
                 get
                 {
                     string estatus = "";
-                    
+
                     // FORMAR LA CADENA DE STATUS PARA IDENTIFICAR EL MENSAJE A DESPLEGAR
                     if (ESTATUS != null) { estatus += ESTATUS; } else { estatus += " "; }
                     if (ESTATUS_C != null) { estatus += ESTATUS_C; } else { estatus += " "; }
@@ -84,6 +86,188 @@ namespace TAT001.Models
                     return Convert.ToInt32(TIEMPO_TRANSCURRIDO);
                 }
             }
+        }
+
+        public class MRLTS
+        {
+            public string CO_CODE { get; set; }
+            public string PAIS { get; set; }
+            public decimal NUMERO_SOLICITUD { get; set; }
+            public DateTime FECHA_SOLICITUD { get; set; }
+            public int PERIODO_CONTABLE { get; set; }
+            public string NUMERO_DOCUMENTO_SAP { get; set; }
+            public int NUMERO_REVERSO_SAP { get; set; }
+            public DateTime FECHA_REVERSO { get; set; }
+            public string PERIODO_CONTABLE_REVERSO { get; set; }
+            public string COMENTARIOS_REVERSO_PROVISION { get; set; }
+            public string TIPO_SOLICITUD { get; set; }
+            public string STATUS { get; set; }
+            public string CONCEPTO_SOLICITUD { get; set; }
+            public DateTime DE { get; set; }
+            public DateTime A { get; set; }
+            public string CLASIFICACION { get; set; }
+            public string NUMERO_CLIENTE { get; set; }
+            public string CLIENTE { get; set; }
+            public decimal MONTO { get; set; }
+            public string MONEDA { get; set; }
+            public decimal TIPO_CAMBIO { get; set; }
+        }
+
+        public class AllowancesB
+        {
+            public string CUENTA_DE_BALANCE { get; set; }
+            public string DESCRIPCION { get; set; }
+            public string FUENTE { get; set; }
+            public int KCMX { get; set; }
+            public int KLCA { get; set; }
+            public int LCCR { get; set; }
+            public int LPKP { get; set; }
+            public int KLSV { get; set; }
+            public int KCAR { get; set; }
+            public int KPRS { get; set; }
+            public int KLCO { get; set; }
+            public int LEKE { get; set; }
+            public int LAGA { get; set; }
+            public int KLCH { get; set; }
+            public bool IS_DOLAR { get; set; }
+        }
+
+        public class AllowancesPL
+        {
+            public string PERIDO { get; set; }
+            public int YEAR { get; set; }
+            public string BU { get; set; }
+            public int PAYER { get; set; }
+            public string CLIENTE { get; set; }
+            public string CANAL { get; set; }
+            public string CATEGORIA { get; set; }
+            public string CD_EN_PROCESO { get; set; }
+            public string CD_ALLOWANCE_TAT { get; set; }
+            public string CD_ALLOWANCE_FACT { get; set; }
+            public string CD_AJUSTES { get; set; }
+            public string CD_TOTALES { get; set; }
+            public string C_EN_PROCESO { get; set; }
+            public string C_ALLOWANCE_TAT { get; set; }
+            public string C_ALLOWANCE_FACT { get; set; }
+            public string C_AJUSTES { get; set; }
+            public string C_TOTALES { get; set; }
+            public string COD_EN_PROCESO { get; set; }
+            public string COD_ALLOWANCE_TAT { get; set; }
+            public string COD_ALLOWANCE_FACT { get; set; }
+            public string COD_AJUSTES { get; set; }
+            public string COD_TOTALES { get; set; }
+            public string DPS_EN_PROCESO { get; set; }
+            public string DPS_ALLOWANCE_TAT { get; set; }
+            public string DPS_ALLOWANCE_FACT { get; set; }
+            public string DPS_AJUSTES { get; set; }
+            public string DPS_TOTALES { get; set; }
+            public string DC_EN_PROCESO { get; set; }
+            public string DC_ALLOWANCE_TAT { get; set; }
+            public string DC_ALLOWANCE_FACT { get; set; }
+            public string DC_AJUSTES { get; set; }
+            public string DC_TOTALES { get; set; }
+            public string ELP_EN_PROCESO { get; set; }
+            public string ELP_ALLOWANCE_TAT { get; set; }
+            public string ELP_ALLOWANCE_FACT { get; set; }
+            public string ELP_AJUSTES { get; set; }
+            public string ELP_TOTALES { get; set; }
+            public string FG_EN_PROCESO { get; set; }
+            public string FG_ALLOWANCE_TAT { get; set; }
+            public string FG_ALLOWANCE_FACT { get; set; }
+            public string FG_AJUSTES { get; set; }
+            public string FG_TOTALES { get; set; }
+            public string GD_EN_PROCESO { get; set; }
+            public string GD_ALLOWANCE_TAT { get; set; }
+            public string GD_ALLOWANCE_FACT { get; set; }
+            public string GD_AJUSTES { get; set; }
+            public string GD_TOTALES { get; set; }
+            public string GP_EN_PROCESO { get; set; }
+            public string GP_ALLOWANCE_TAT { get; set; }
+            public string GP_ALLOWANCE_FACT { get; set; }
+            public string GP_AJUSTES { get; set; }
+            public string GP_TOTALES { get; set; }
+            public string LD_EN_PROCESO { get; set; }
+            public string LD_ALLOWANCE_TAT { get; set; }
+            public string LD_ALLOWANCE_FACT { get; set; }
+            public string LD_AJUSTES { get; set; }
+            public string LD_TOTALES { get; set; }
+            public string MS_EN_PROCESO { get; set; }
+            public string MS_ALLOWANCE_TAT { get; set; }
+            public string MS_ALLOWANCE_FACT { get; set; }
+            public string MS_AJUSTES { get; set; }
+            public string MS_TOTALES { get; set; }
+            public string R_EN_PROCESO { get; set; }
+            public string R_ALLOWANCE_TAT { get; set; }
+            public string R_ALLOWANCE_FACT { get; set; }
+            public string R_AJUSTES { get; set; }
+            public string R_TOTALES { get; set; }
+            public string TP_EN_PROCESO { get; set; }
+            public string TP_ALLOWANCE_TAT { get; set; }
+            public string TP_ALLOWANCE_FACT { get; set; }
+            public string TP_AJUSTES { get; set; }
+            public string TP_TOTALES { get; set; }
+            public string SIS_EN_PROCESO { get; set; }
+            public string SIS_ALLOWANCE_TAT { get; set; }
+            public string SIS_ALLOWANCE_FACT { get; set; }
+            public string SIS_AJUSTES { get; set; }
+            public string SIS_TOTALES { get; set; }
+            public string SO_EN_PROCESO { get; set; }
+            public string SO_ALLOWANCE_TAT { get; set; }
+            public string SO_ALLOWANCE_FACT { get; set; }
+            public string SO_AJUSTES { get; set; }
+            public string SO_TOTALES { get; set; }
+            public string TPIS_EN_PROCESO { get; set; }
+            public string TPIS_ALLOWANCE_TAT { get; set; }
+            public string TPIS_ALLOWANCE_FACT { get; set; }
+            public string TPIS_AJUSTES { get; set; }
+            public string TPIS_TOTALES { get; set; }
+            public string TPO_EN_PROCESO { get; set; }
+            public string TPO_ALLOWANCE_TAT { get; set; }
+            public string TPO_ALLOWANCE_FACT { get; set; }
+            public string TPO_AJUSTES { get; set; }
+            public string TPO_TOTALES { get; set; }
+            public string U_EN_PROCESO { get; set; }
+            public string U_ALLOWANCE_TAT { get; set; }
+            public string U_ALLOWANCE_FACT { get; set; }
+            public string U_AJUSTES { get; set; }
+            public string U_TOTALES { get; set; }
+            public string WA_EN_PROCESO { get; set; }
+            public string WA_ALLOWANCE_TAT { get; set; }
+            public string WA_ALLOWANCE_FACT { get; set; }
+            public string WA_AJUSTES { get; set; }
+            public string WA_TOTALES { get; set; }
+            public int PROCESO_TAT_TOTAL { get; set; }
+            public int ALLOWANCE_TAT_TOTAL { get; set; }
+            public int ALLOWANCE_FACT_TOTAL { get; set; }
+            public int AJUSTES { get; set; }
+            public int ALLOWANCE_TOTALES { get; set; }
+        }
+
+        public class Concentrado
+        {
+            internal List<FLUJO> workflow;
+            internal FLUJO acciones;
+            internal List<DOCUMENTOA> files;
+            internal string pais;
+            internal List<TS_FORM> ts;
+            internal List<DOCUMENTOT> tts;
+            internal List<CARTAP> cartap;
+            internal string ultMod;
+            internal string miles;
+            internal string dec;
+
+            public DOCUMENTO documento { set; get; }
+            public FLUJO flujo { set; get; }
+            public string Title { get; set; }
+            public List<TSOL> TSOL_RELA { get; set; }
+            public CANAL CANAL { get; set; }
+            public decimal CUENTA_ABONO { get; set; }
+            public decimal CUENTA_CARGO { get; set; }
+            public decimal CUENTA_CLEARING { get; set; }
+            public decimal CUENTA_LIMITE { get; set; }
+            public string CUENTA_CARGO_NOMBRE { get; set; }
+            public PRESUPUESTO_MOD PRESUPUESTO { get; set; }
+            public string PROVEEDOR_NOMBRE { get; set; }
         }
     }
 }
