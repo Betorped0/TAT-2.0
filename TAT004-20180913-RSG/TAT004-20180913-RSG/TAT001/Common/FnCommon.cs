dc88159098be951c0e8a1f88d0ff1f76dcc33c35
Mary26 <maribel.cruz.tellez@gmail.com>
2018-10-09 15:05:12 -0500

    Se agrega FnCommon, DropDownTree(se inicia desarrollo), se actualiza condicion para manejo sinFecha.

---
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Common/FnCommon.cs
@@ -0,0 +1,160 @@
+﻿
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TAT001.Entities;
+using TAT001.Models;
+
+namespace TAT001.Common
+{
+    public static class FnCommon
+    {
+        public static USUARIO ObtenerUsuario(TAT001Entities db, string user_id)
+        {
+            return db.USUARIOs.Where(a => a.ID.Equals(user_id)).FirstOrDefault();
+        }
+        public static string ObtenerSprasId(TAT001Entities db, string user_id)
+        {
+            return db.USUARIOs.Where(a => a.ID.Equals(user_id)).FirstOrDefault().SPRAS_ID;
+        }
+        public static string ObtenerTextoMnj(TAT001Entities db, int pagina_id, string campo_id, string user_id)
+        {
+            string spras_id = ObtenerSprasId(db, user_id);
+            string texto = "";
+            if (db.TEXTOes.Any(a => (a.PAGINA_ID.Equals(pagina_id) && a.SPRAS_ID.Equals(spras_id) && a.CAMPO_ID.Equals(campo_id))))
+            {
+                texto = db.TEXTOes.Where(a => (a.PAGINA_ID.Equals(pagina_id) && a.SPRAS_ID.Equals(spras_id) && a.CAMPO_ID.Equals(campo_id))).FirstOrDefault().TEXTOS;
+            }
+            return texto;
+        }
+        public static void ObtenerConfPage(TAT001Entities db, int pagina_id, string user_id, ControllerBase controller)
+        {
+            var user = FnCommon.ObtenerUsuario(db, user_id);
+            controller.ViewBag.permisos = db.PAGINAVs.Where(a => a.ID.Equals(user.ID)).ToList();
+            controller.ViewBag.carpetas = db.CARPETAVs.Where(a => a.USUARIO_ID.Equals(user.ID)).ToList();
+            controller.ViewBag.usuario = user;
+            controller.ViewBag.rol = user.PUESTO.PUESTOTs.Where(a => a.SPRAS_ID.Equals(user.SPRAS_ID)).FirstOrDefault().TXT50;
+            controller.ViewBag.Title = db.PAGINAs.Where(a => a.ID.Equals(pagina_id)).FirstOrDefault().PAGINATs.Where(b => b.SPRAS_ID.Equals(user.SPRAS_ID)).FirstOrDefault().TXT50;
+            controller.ViewBag.warnings = db.WARNINGVs.Where(a => (a.PAGINA_ID.Equals(pagina_id) || a.PAGINA_ID.Equals(0)) && a.SPRAS_ID.Equals(user.SPRAS_ID)).ToList();
+            controller.ViewBag.textos = db.TEXTOes.Where(a => (a.PAGINA_ID.Equals(pagina_id) || a.PAGINA_ID.Equals(0)) && a.SPRAS_ID.Equals(user.SPRAS_ID)).ToList();
+            controller.ViewBag.spras_id = user.SPRAS_ID;
+        }
+        public static List<SelectListItem> ObtenerCmbTiposSolicitud(TAT001Entities db, string spras_id, string id)
+        {
+          return   db.TSOLs
+                    .Join(db.TSOLTs, s => s.ID, st => st.TSOL_ID, (s, st) => st)
+                    .Where(x => x.SPRAS_ID == spras_id && (x.TSOL_ID == id || id == null))
+                    .Select(x => new SelectListItem
+                    {
+                        Value = x.TSOL_ID,
+                        Text = (x.TSOL_ID + "-" + x.TXT50)
+                    }).ToList();
+        }
+        public static List<SelectListItem> ObtenerCmbSociedades(TAT001Entities db, string id)
+        {
+            return db.SOCIEDADs
+                .Where(x => x.BUKRS == id || id == null)
+                .Select(x => new SelectListItem
+                {
+                    Value = x.BUKRS,
+                    Text = x.BUKRS
+                }).ToList();
+        }
+        public static List<SelectListItem> ObtenerCmbPeriodos(TAT001Entities db, string spras_id, int? id)
+        {
+            return db.PERIODOes
+                .Join(db.PERIODOTs, p => p.ID, pt => pt.PERIODO_ID, (p, pt) => pt)
+                .Where(x => x.SPRAS_ID == spras_id && (x.PERIODO_ID == id || id == null))
+                .Select(x => new SelectListItem
+                {
+                    Value = x.PERIODO_ID.ToString(),
+                    Text = (x.PERIODO_ID.ToString() + " - " + x.TXT50)
+                }).ToList();
+        }
+        public static List<SelectListItem> ObtenerCmbUsuario(TAT001Entities db, string id)
+        {
+            return db.USUARIOs
+                .Where(x => x.ID == id || id == null)
+                .Select(x => new SelectListItem
+                {
+                    Value = x.ID,
+                    Text = (x.NOMBRE + " " + x.APELLIDO_P + " " + (x.APELLIDO_M == null ? "" : x.APELLIDO_M))
+                }).ToList();
+        }
+        public static List<SelectListItem> ObtenerCmbTabs(TAT001Entities db, string spras_id, string id)
+        {
+            return db.TABs
+                .Join(db.TEXTOes, ta => ta.ID, te => te.CAMPO_ID, (ta, te) => te)
+                .Where(x => x.SPRAS_ID == spras_id && x.PAGINA_ID == 202 && (x.CAMPO_ID == id || id == null))
+                .Select(x => new SelectListItem
+                {
+                    Value = x.CAMPO_ID,
+                    Text = x.TEXTOS
+                }).ToList();
+        }
+        public static List<SelectListItem> ObtenerCmbCamposPoTabId(TAT001Entities db, string spras_id, string tab_id, string id)
+        {
+            return db.TAB_CAMPO
+                    .Where(x => x.TAB_ID == tab_id)
+                    .Join(db.TEXTOes, tc => tc.CAMPO_ID, te => te.CAMPO_ID, (ta, te) => te)
+                    .Where(x => x.SPRAS_ID == spras_id && x.PAGINA_ID == 202 && (x.CAMPO_ID == id || id == null))
+                    .Select(x => new SelectListItem
+                    {
+                        Value = x.CAMPO_ID,
+                        Text = x.TEXTOS
+                    }).ToList();
+        }
+        public static List<SelectTreeItem> ObtenerTreeTiposSolicitud(TAT001Entities db, string spras_id, string id)
+        {
+            List<SelectTreeItem> tree = new List<SelectTreeItem>();
+            db.TSOL_GROUP
+            .Where(x => x.ID_PADRE == null && x.TIPO_PADRE == null)
+            .ToList().ForEach(x =>
+            {
+                tree.Add(new SelectTreeItem
+                {
+                    value = x.ID + "_" + x.TIPO,
+                    text = x.DESCRIPCION,
+                    expanded = true,
+                    items = ObtenerItemsSelectTree(db, x.ID, x.TIPO, spras_id, id)
+                });
+            });
+            return tree;
+        }
+
+       static List<SelectTreeItem> ObtenerItemsSelectTree(TAT001Entities db, string id_padre, string tipo_padre, string spras_id, string id)
+        {
+            List<SelectTreeItem> tree = new List<SelectTreeItem>();
+            db.TSOL_GROUP
+                .Where(x => x.ID_PADRE == id_padre && x.TIPO_PADRE == tipo_padre)
+                .ToList().ForEach(x =>
+                {
+                    SelectTreeItem item = new SelectTreeItem();
+                    item.value = x.ID + "_" + x.TIPO;
+                    item.text = x.DESCRIPCION;
+                    item.expanded = true;
+                    item.items = ObtenerItemsSelectTree(db, x.ID, x.TIPO, spras_id,id);
+                    if (item.items.Count()==0)
+                    {
+                        db.TSOL_TREE
+                        .Where(y => y.TSOL_GROUP_ID == x.ID && y.TSOL_GROUP_TIPO == x.TIPO)
+                        .Join(db.TSOLTs, tst => tst.TSOL_ID, st => st.TSOL_ID, (tst, st) => st)
+                        .Where(y => y.SPRAS_ID == spras_id && (y.TSOL_ID == id || id == null))
+                        .ToList().ForEach(y =>
+                        {
+                            item.items.Add(new SelectTreeItem
+                            {
+                                value = y.TSOL_ID,
+                                text = y.TXT50,
+                                expanded = true
+                            });
+                        });
+                    }
+                    tree.Add(item);
+                });
+            return tree;
+        }
+
+    }
+}
