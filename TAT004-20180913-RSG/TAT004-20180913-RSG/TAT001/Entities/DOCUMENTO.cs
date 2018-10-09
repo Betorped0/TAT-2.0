50f700f01eeffb3d106a33fb40c3db893cd3333f
Rogelio Sánchez Gómez <36237248+sf-rogeliosanchez@users.noreply.github.com>
2018-10-09 16:12:14 -0500

    20181009-RSG
    Cambios tablas Maribel

--- TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Entities/DOCUMENTO.cs
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Entities/DOCUMENTO.cs
@@ -27,7 +27,10 @@ namespace TAT001.Entities
             this.DOCUMENTOTS = new HashSet<DOCUMENTOT>();
             this.FLUJOes = new HashSet<FLUJO>();
         }
-    
+        public List<TAT001.Models.DOCUMENTOP_MOD> DOCUMENTOP { get; set; }
+        public List<DOCUMENTOF> DOCUMENTOF { get; set; }
+        public List<DOCUMENTOREC> DOCUMENTOREC { get; set; }
+        public List<DOCUMENTORAN> DOCUMENTORAN { get; set; }
         public decimal NUM_DOC { get; set; }
         public string TSOL_ID { get; set; }
         public string TALL_ID { get; set; }
@@ -96,10 +99,6 @@ namespace TAT001.Entities
         public string SPART { get; set; }
         public Nullable<int> PUESTO_ID { get; set; }
         public string GALL_ID { get; set; }
-        public List<TAT001.Models.DOCUMENTOP_MOD> DOCUMENTOP { get; set; }
-        public List<DOCUMENTOF> DOCUMENTOF { get; set; }
-        public List<DOCUMENTOREC> DOCUMENTOREC { get; set; }
-        public List<DOCUMENTORAN> DOCUMENTORAN { get; set; }
         public Nullable<int> CONCEPTO_ID { get; set; }
         public string DOCUMENTO_SAP { get; set; }
         public Nullable<decimal> PORC_APOYO { get; set; }
