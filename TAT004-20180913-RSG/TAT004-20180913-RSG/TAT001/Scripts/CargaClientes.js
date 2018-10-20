7b4dbdc87c2c460d32a746782624e1815395a393
Betorped0 <rmartinezg0901@alumno.ipn.mx>
2018-10-20 13:02:17 -0500

    disable a campos sensibles

--- TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Scripts/CargaClientes.js
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Scripts/CargaClientes.js
@@ -386,8 +386,8 @@ function addRow(t, POS, br, cc, p, c, nc, n0, n1, n2, n3, n4, n5, n6, n7, v, b, 
         "<label><input class='input_bor' type='checkbox' id='' name='bor' onclick='checkoff();' value='" + br + "'><span></span></label>",
         "<input class='input_coc' style='font-size:12px;' type='text' id='' name='coc' value='" + cc + "' onchange='Comprobar()'><span  style='display: none;'>" + cc + "</span>",
         "<input class='input_pai' style='font-size:12px;' type='text' id='' name='pai' value='" + p + "' onchange='Comprobar()'><span  style='display: none;'>" + p + "</span>",
-        "<input class='input_cli' style='font-size:12px;' type='text' id='' name='cli' value='" + c + "' onchange='Comprobar()'><span  style='display: none;'>" + c + "</span>",
-        "<input class='input_noc' style='font-size:12px;' type='text' id='' name='noc' value='" + nc + "' onchange='Comprobar()'><span  style='display: none;'>" + nc + "</span>",
+        "<input class='input_cli' disabled='true' style='font-size:12px;' type='text' id='' name='cli' value='" + c + "' onchange='Comprobar()'><span  style='display: none;'>" + c + "</span>",
+        "<input class='input_noc' disabled='true' style='font-size:12px;' type='text' id='' name='noc' value='" + nc + "' onchange='Comprobar()'><span  style='display: none;'>" + nc + "</span>",
         "<input class='input_ni0' style='font-size:12px;' type='text' id='' name='ni0' value='" + n0 + "' onchange='Comprobar()'><span  style='display: none;'>" + n0 + "</span>",
         "<input class='input_ni1' style='font-size:12px;' type='text' id='' name='ni1' value='" + n1 + "' onchange='Comprobar()'><span  style='display: none;'>" + n1 + "</span>",
         "<input class='input_ni2' style='font-size:12px;' type='text' id='' name='ni2' value='" + n2 + "' onchange='Comprobar()'><span  style='display: none;'>" + n2 + "</span>",
@@ -397,7 +397,7 @@ function addRow(t, POS, br, cc, p, c, nc, n0, n1, n2, n3, n4, n5, n6, n7, v, b, 
         "<input class='input_ni6' style='font-size:12px;' type='text' id='' name='ni6' value='" + n6 + "' onchange='Comprobar()'><span  style='display: none;'>" + n6 + "</span>",
         "<input class='input_ni7' style='font-size:12px;' type='text' id='' name='ni7' value='" + n7 + "' onchange='Comprobar()'><span  style='display: none;'>" + n7 + "</span>",
         "<input class='input_ven' style='font-size:12px;' type='text' id='' name='ven' value='" + v + "' onchange='Comprobar()'><span  style='display: none;'>" + v + "</span>",
-        "<input class='input_ban' style='font-size:12px;' type='text' id='' name='ban' value='" + b + "' onchange='Comprobar()'><span  style='display: none;'>" + b + "</span>",
+        "<input class='input_ban' disabled='true' style='font-size:12px;' type='text' id='' name='ban' value='" + b + "' onchange='Comprobar()'><span  style='display: none;'>" + b + "</span>",
         "<input class='input_baa' style='font-size:12px;' type='text' id='' name='baa' value='" + ba + "' onchange='Comprobar()'><span  style='display: none;'>" + ba + "</span>",
         "<input class='input_can' style='font-size:12px;' type='text' id='' name='can' value='" + ca + "' onchange='Comprobar()'><span  style='display: none;'>" + ca+ "</span>",
         "<input class='input_exp' style='font-size:12px;' type='text' id='' name='exp' value='" + ex + "' onchange='Comprobar()'><span  style='display: none;'>" + ex + "</span>",
@@ -455,6 +455,9 @@ function Carga() {
                     dataType: "json",
                     success: function () {
                         console.log(datos);
+                        $('#table > tbody > tr').each(function () {
+                            $(this).children().children().val("");
+                        });
                     },
                     error: function (request, status, error) {
                         console.log(request.responseText);
@@ -462,6 +465,7 @@ function Carga() {
                 });
                 M.toast({ html: 'Se agregaron los nuevos registros' });
                 //window.location.replace("/Clientes/Index");
+                
             }
             else
                 M.toast({ html: 'Los niveles 1 y 6 no pueden quedar vacios' });
