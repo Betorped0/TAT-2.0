835f157b8d87dfb155fd77c5e8729d6b331cff23
Mary26 <maribel.cruz.tellez@gmail.com>
2018-11-04 14:43:01 -0600

    Se agrega impuesto en vistas de Solicitudes

--- TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Scripts/listas.js
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Scripts/listas.js
@@ -142,3 +142,73 @@ function cierre() {
     });
     return bool;
 }
+function asignarSolicitud(num, num2) {
+    num = toNum(num);
+    num2 = toNum(num2);
+    var soc = $("#sociedad_id").val();
+    var tsol = (!$("#TSOL_ID").val() ? $("#tsol_id").val() : $("#TSOL_ID").val());
+    var esCategoriaUnica = false;
+    if ($("#select_dis").val()=="C"){
+        $("#table_dis > tbody  > tr[role='row']").each(function (indx, row) {
+            var cat = row.cells.item(5).textContent;
+            var _xxx = $.parseJSON($('#catmat').val());
+            for (var i = 0; i < _xxx.length; i++) {
+                if (cat === _xxx[i].DESCRIPCION && _xxx[i].UNICA === true) {
+                        esCategoriaUnica = true;
+                }
+            }
+        });
+    }
+    if (true) {
+        $.ajax({
+            type: "POST",
+            url: 'getSolicitud',
+            dataType: "json",
+            data: { num: num, monto: num2, tsol_id: tsol, sociedad_id: soc, esCategoriaUnica: esCategoriaUnica },
+
+            success: function (data) {
+
+                if (data !== null || data !== "") {
+                    if (data.S_NUM == "") {
+                        var num2 = $('#monto_doc_md').val();
+                        $('#s_montob').text(num2);
+                        $('#s_montop').text("-");
+                        $('#s_montoa').text("-");
+                        $('#s_rema').text(num2);
+                        $('#s_rema').text("-");//RSG 09.07.2018
+                        $('#s_impa').text(toShow(data.S_IMPA));
+                        $('#s_impb').text("-");
+                        $('#s_impc').text("-");
+                        $('#s_ret').text("-");
+                        $('#s_total').text(num2);
+                    }
+                    else {
+                        $('#s_montob').text(toShow(data.S_MONTOB));
+                        $('#s_montop').text(toShow(data.S_MONTOP));
+                        if (data.S_MONTOA != "-")
+                            $('#s_montoa').text(toShow(data.S_MONTOA));
+                        if (data.S_REMA < 0) {
+                            $('#s_rema').text(toShow(data.S_REMA));
+                            document.getElementById("a4").classList.add("red");
+                            document.getElementById("a4").classList.add("white-text");
+                        }
+                        else {
+                            $('#s_rema').text(toShow(data.S_REMA));
+                            document.getElementById("a4").classList.remove("red");
+                            document.getElementById("a4").classList.remove("white-text");
+                        }
+                        $('#s_impa').text(data.S_IMPA);
+                        $('#s_impb').text(data.S_IMPB);
+                        $('#s_impc').text(data.S_IMPC);
+                        $('#s_ret').text(toShow(data.S_RET));
+                        $('#s_total').text(toShow(data.S_TOTAL));
+                    }
+                }
+            },
+            error: function (xhr, httpStatusMessage, customErrorMessage) {
+                M.toast({ html: httpStatusMessage });
+            },
+            async: false
+        });
+    }
+}
