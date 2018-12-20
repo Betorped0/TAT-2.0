a8eab5ff13bc7ab5679bb85c440fa4b800bb7473
Mary26 <maribel.cruz.tellez@gmail.com>
2018-12-20 15:12:30 -0600

    Clientes

--- TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Scripts/CargaClientes.js
+++ TAT004-20180913-RSG/TAT004-20180913-RSG/TAT001/Scripts/CargaClientes.js
@@ -204,11 +204,12 @@ function Carga() {
     if (doc > 0 || cliente !== null || cliente !== "cli=") {
         if (message === "" || message === null || message.endsWith('mes=')) {
             if (us1 !== "" && us1 !== "ni1=" && us1 !== null && us6 !== "" && us6 !== "ni6=" && us6 !== null) {
+
+                document.getElementById("loader").style.display = "flex";
                 var table = $('#table').DataTable();
                 table.destroy();
                 habilitar();
                 var datos = $('#tabla').serializeArray();
-                document.getElementById("loader").style.display = "flex";
                 $.ajax({
                     type: "POST",
                     url: 'Agregar',
